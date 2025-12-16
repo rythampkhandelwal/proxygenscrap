37e39fe3b1a4c063d32f8c34ba14d336ca8587fb
Rytham Khandelwal <rythampkhandelwal@gmail.com>
2025-12-16 16:48:16 +0530

    Update print statement from 'Hello' to 'Goodbye'

--- bot.py
+++ bot.py
@@ -47,7 +47,7 @@ DEFAULT_CHECK_USER_AGENT = DEFAULT_SCRAPE_USER_AGENT
 DEFAULT_ALLOW_INSECURE_SSL = True
 
 # Telegram bot
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "8190937825:AAExpRnZvSqBd_Wd0DsnhJghwpm3tv3hgJ0")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8190937825:AAF56JvnqeIie9VWOQXB5H2o6g0Y6YVcUbc")
 DEFAULT_PROTOCOLS = ["http", "https", "socks4", "socks5"]
 OUTPUT_BASE = Path("./out")
 SCRAPED_DIR = OUTPUT_BASE / "scraped"
@@ -915,3 +915,4 @@ if __name__ == "__main__":
     except KeyboardInterrupt:
         print("\n⏹️  Bot stopped by user.")
         sys.exit(0)
+
