48c5e21ecb65f6c6f1a50c5d7d6d25ab154b9349
Rytham Khandelwal <rythampkhandelwal@gmail.com>
2025-12-16 16:52:47 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -47,7 +47,7 @@ DEFAULT_CHECK_USER_AGENT = DEFAULT_SCRAPE_USER_AGENT
 DEFAULT_ALLOW_INSECURE_SSL = True
 
 # Telegram bot
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "8190937825:AAF56JvnqeIie9VWOQXB5H2o6g0Y6YVcUbc")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8190937825:AAF082U0jKiD7B5o6p__dFrT4kBREQyfj4c")
 DEFAULT_PROTOCOLS = ["http", "https", "socks4", "socks5"]
 OUTPUT_BASE = Path("./out")
 SCRAPED_DIR = OUTPUT_BASE / "scraped"
@@ -915,4 +915,5 @@ if __name__ == "__main__":
     except KeyboardInterrupt:
         print("\n⏹️  Bot stopped by user.")
         sys.exit(0)
+
 
